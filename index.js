ec0392ac6cd75283c73d8d914c705dffa29f316e
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-07-25 17:14:52 +0300

    added code for index js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(driverObjects, name) {
+    return driverObjects.filter(driver => driver.name === name);
+  }
+  
+  WaveShaperNode
